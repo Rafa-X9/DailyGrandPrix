8afe5b33d321f52ac16605c5179ad4e0d189e93f
RafaX9 <rafaeldealencarfloriano@gmail.com>
2026-02-14 22:19:00 -0400

    Part 1 of updating documentation

--- DailyGrandPrix/obj/Debug/net9.0/DailyGrandPrix.AssemblyInfo.cs
+++ DailyGrandPrix/obj/Debug/net9.0/DailyGrandPrix.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DailyGrandPrix")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2be96cf7d8795abc14f868fbd66d7845c7a9b7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c61ab7282bb95443303ced4253664c2a75c32e0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DailyGrandPrix")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DailyGrandPrix")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
